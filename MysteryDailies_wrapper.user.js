943343dd3a1bd40004c98775d1ce7575f43c8c7b
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-27 03:01:14 +0000

    Create MysteryDailies_wrapper.user.js

---
+++ MysteryDailies_wrapper.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Mystery Dailies
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Send daily farm and resource stats to Discord webhook
+// @author       Mystery Inc.
+// @icon         https://i.ibb.co/x8JQX8yS/ex1.png
+// @match        https://*.tribalwars.com.pt/game.php*
+// @require      https://raw.githubusercontent.com/jpsilva95/MysteryInc/main/MysteryDailies.js
+// @updateURL    https://raw.githubusercontent.com/jpsilva95/MysteryInc/main/MysteryDailies_wrapper.user.js
+// @downloadURL  https://raw.githubusercontent.com/jpsilva95/MysteryInc/main/MysteryDailies_wrapper.user.js
+// @grant        GM_xmlhttpRequest
+// @connect      discord.com
+// ==/UserScript==
+
+//   © 2025 Mystery Inc.©. All Rights Reserved
