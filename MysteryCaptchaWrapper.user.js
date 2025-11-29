d2ceb094c91de5a2096730ecda401f58f77928b2
João Paulo Silva <132902307+jpsilva95@users.noreply.github.com>
2025-11-29 03:07:32 +0000

    Create MysteryCaptchaWrapper.user.js

---
+++ MysteryCaptchaWrapper.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         MysteryInc Captcha Discord and Telegram Notification with Timer
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Notifies Discord/Telegram when a captcha appears and redirects after timer
+// @author       Team Rocket
+// @icon         https://i.ibb.co/x8JQX8yS/ex1.png
+// @include      https://*.tribalwars.com.pt/game.php*
+// @require      https://raw.githubusercontent.com/jpsilva95/MysteryInc/main/MysteryCaptcha.js
+// @updateURL    https://raw.githubusercontent.com/jpsilva95/MysteryInc/main/MysteryCaptchaWrapper.user.js
+// @downloadURL  https://raw.githubusercontent.com/jpsilva95/MysteryInc/main/MysteryCaptchaWrapper.user.js
+// @grant        none
+// ==/UserScript==
+
+//   © 2025 Team Rocket. All Rights Reserved
